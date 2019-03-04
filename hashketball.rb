bba6ae82bd082bcc57097e7a042231305448c74f
Francisco Costa <francisco@fcosta.pt>
2019-03-04 22:59:37 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,173 @@
-# Write your code here!
+require 'pry'
 
+def game_hash
+  game_hash = {
+    home: {
+      team_name: "Brooklyn Nets",
+      colors: ["Black", "White"],
+      players:{
+        "Alan Anderson": {
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 1
+        },
+        "Regie Evans": {
+          number: 30,
+          shoe: 14,
+          points: 12,
+          rebounds: 12,
+          assists: 12,
+          steals: 12,
+          blocks: 12,
+          slam_dunks: 7
+        },
+        "Brook Lopez": {
+          number: 11,
+          shoe: 17,
+          points: 17,
+          rebounds: 19,
+          assists: 10,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 15
+        },
+        "Mason Plumlee": {
+          number: 1,
+          shoe: 19,
+          points: 26,
+          rebounds: 12,
+          assists: 6,
+          steals: 3,
+          blocks: 8,
+          slam_dunks: 5
+        },
+        "Jason Terry": {
+          number: 31,
+          shoe: 15,
+          points: 19,
+          rebounds: 2,
+          assists: 2,
+          steals: 4,
+          blocks: 11,
+          slam_dunks: 1
+        }
+      }
+    },
+    away: {
+      team_name: "Charlotte Hornets",
+      colors: ["Turquoise", "Purple"],
+      players: {
+        "Jeff Adrien": {
+          number: 4,
+          shoe: 18,
+          points: 10,
+          rebounds: 1,
+          assists: 1,
+          steals: 2,
+          blocks: 7,
+          slam_dunks: 2
+        },
+        "Bismak Biyombo": {
+          number: 0,
+          shoe: 16,
+          points: 12,
+          rebounds: 4,
+          assists: 7,
+          steals: 7,
+          blocks: 15,
+          slam_dunks: 10
+        },
+        "DeSagna Diop": {
+          number: 2,
+          shoe: 14,
+          points: 24,
+          rebounds: 12,
+          assists: 12,
+          steals: 4,
+          blocks: 5,
+          slam_dunks: 5
+        },
+        "Ben Gordon": {
+          number: 8,
+          shoe: 15,
+          points: 33,
+          rebounds: 3,
+          assists: 2,
+          steals: 1,
+          blocks: 1,
+          slam_dunks: 0
+        },
+        "Brendan Haywood": {
+          number: 33,
+          shoe: 15,
+          points: 6,
+          rebounds: 12,
+          assists: 12,
+          steals: 22,
+          blocks: 5,
+          slam_dunks: 12
+        }
+      }
+    }
+  }
+end
 
+def num_points_scored(name)
+  array =[]
+  game_hash.collect do |team, specs|
+    specs.collect do |specs, values|
+      if specs == :players
+        values.collect do |player, value|
+          if player.to_s == name
+            value.collect do |key, int|
+              if key == :points
+                array << int
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+  array
+  #tried array[0] and it didn't work
+end
 
+def shoe_size(name)
+  array =[]
+  game_hash.collect do |team, specs|
+    specs.collect do |specs, values|
+      if specs == :players
+        values.collect do |player, value|
+          if player.to_s == name
+            value.collect do |key, int|
+              if key == :shoe
+                array << int
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+  array
+  #tried array[0] and it didn't work
+end
 
-
-
-
-
-
-
+def team_colors(team_name)
+  colors = []
+  game_hash.collect do |team, details|
+    details.collect do |specs, values|
+      if values.to_s == team_name
+        colors << details[:colors]
+      end
+    end
+  end
+  colors.compact
+  colors[0]
+end
